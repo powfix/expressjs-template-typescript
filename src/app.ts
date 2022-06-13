666f89d3cd748984a648c4142d8cf7635b67aa4a
POWFIX <powfix@gmail.com>
2022-06-13 17:57:18 +0900

    parseInt() env.PORT

--- src/app.ts
+++ src/app.ts
@@ -31,7 +31,7 @@ app.get('/', (req, res) => {
 
 app.use(require('./routes'));
 
-const PORT = process.env.PORT || 3000;
+const PORT: number = parseInt(process.env.PORT as string, 10) || 3000;
 app.listen(PORT, () => {
   console.log(`
   ######################################
