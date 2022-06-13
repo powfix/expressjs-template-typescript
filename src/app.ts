88f8f38d42a1b69308f5b15d2dda24cbc18507e7
POWFIX <powfix@gmail.com>
2022-06-13 17:30:38 +0900

    Middlewares

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,20 @@ import express from 'express';
 import helmet from "helmet";
 import nocache from "nocache";
 import cors from "cors";
+import bodyParser from "body-parser";
 
 const app = express();
+
+// Middlewares
 app.use(helmet());
 app.use(nocache());
 app.use(cors());
+
+// Body Parser
+app.use(bodyParser.raw());
+app.use(bodyParser.text({limit: '4mb'}));
+app.use(bodyParser.json({limit: '4mb'}));
+app.use(bodyParser.urlencoded({extended: true, limit: '4mb'}));
 
 app.get('/', (req, res) => {
   res.send('Hello world');
